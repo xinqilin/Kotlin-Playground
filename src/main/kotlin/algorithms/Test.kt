9bf962b149bacaae3b73905ffdc0a93ea65308d4
bill.lin <zzx123bill@gmail.com>
2024-04-08 23:49:06 +0800

    modify TypeChecking

--- src/main/kotlin/algorithms/Test.kt
+++ src/main/kotlin/algorithms/Test.kt
@@ -1,25 +1,36 @@
 package algorithms
 
+import java.util.*
 
-internal class Solution {
-    fun subsets(nums: IntArray): List<List<Int>> {
-        val ans: ArrayList<List<Int>> = ArrayList()
-        val index = 0
-        backtracking(ans, ArrayList(), nums, index)
+
+class Test {
+    fun diagonalSort(mat: Array<IntArray>): Array<IntArray> {
+        val row = mat.size
+        val col = mat[0].size
+        val ans = Array(row) { IntArray(col) }
+        for (i in 0 until row) {
+            sort(ans, mat, i, 0, row, col)
+        }
+        for (i in 1 until col) {
+            sort(ans, mat, 0, i, row, col)
+        }
         return ans
     }
 
-    private fun backtracking(ans: MutableList<List<Int>>, list: MutableList<Int>, nums: IntArray, index: Int) {
-        if (index == nums.size) {
-            ans.add(ArrayList(list))
-            return
+    private fun sort(ans: Array<IntArray>, mat: Array<IntArray>, row: Int, col: Int, rowLimit: Int, colLimit: Int) {
+        var row = row
+        var col = col
+        val queue: Queue<Int> = PriorityQueue { a: Int, b: Int -> b - a }
+        val positionR = row
+        val positionC = col
+        while (col < colLimit && row < rowLimit) {
+            queue.offer(mat[row++][col++])
         }
-        list.add(nums[index])
-        backtracking(ans, list, nums, index + 1)
-        list.removeLast()
-        backtracking(ans, list, nums, index + 1)
 
-
+        while (row != positionR && col != positionC) {
+            ans[--row][--col] = queue.poll()
+        }
     }
-
 }
+
+
--- src/main/kotlin/basic/TypeChecking.kt
+++ src/main/kotlin/basic/TypeChecking.kt
@@ -1,85 +1,85 @@
 package basic
 
-interface TypeChecking {
-    fun walk() = "$name: walk"
-    fun fly() = "$name: fly"
-}
-
-class HouseFly : TypeChecking
-
-class Flea : Insect {
-    override fun fly() =
-        throw Exception("Flea cannot fly")
-
-    fun crawl() = "Flea: crawl"
-}
-
-fun TypeChecking.basic() =
-    walk() + " " +
-            if (this is Flea)
-                crawl()
-            else
-                fly()
-
-interface SwimmingInsect : TypeChecking {
-    fun swim() = "$name: swim"
-}
-
-interface WaterWalker : TypeChecking {
-    fun walkWater() =
-        "$name: walk on water"
-}
-
-class WaterBeetle : SwimmingInsect
-class WaterStrider : WaterWalker
-class WhirligigBeetle :
-    SwimmingInsect, WaterWalker
-
-fun TypeChecking.water() =
-    when (this) {
-        is SwimmingInsect -> swim()
-        is WaterWalker -> walkWater()
-        else -> "$name: drown"
-    }
-
-fun main() {
-    val insects = listOf(
-        HouseFly(), Flea(), WaterStrider(),
-        WaterBeetle(), WhirligigBeetle()
-    )
-    insects.map { it.basic() }  // =>
-            "[HouseFly: walk HouseFly: fly, " +
-            "Flea: walk Flea: crawl, " +
-            "WaterStrider: walk WaterStrider: fly, " +
-            "WaterBeetle: walk WaterBeetle: fly, " +
-            "WhirligigBeetle: walk " +
-            "WhirligigBeetle: fly]"
-    insects.map { it.water() }  // =>
-            "[HouseFly: drown, Flea: drown, " +
-            "WaterStrider: walk on water, " +
-            "WaterBeetle: swim, " +
-            "WhirligigBeetle: swim]"
-}
-
-interface Shape {
-    fun draw(): String
-}
-
-class Triangle : Shape {
-    override fun draw() = "Triangle: Draw"
-    fun rotate() = "Triangle: Rotate"
-}
-
-fun turn2(s: Shape) = when (s) {
-    is Square -> s.rotate()
-    is Triangle -> s.rotate()
-    else -> ""
-}
-
-fun main() {
-    val shapes =
-        listOf(Circle(), Square(), Triangle())
-    shapes.map { it.draw() }  // => "[Circle: Draw, Square: Draw, Triangle: Draw]"
-    shapes.map { turn(it) }  // => "[, Square: Rotate, ]"
-    shapes.map { turn2(it) }  // => "[, Square: Rotate, Triangle: Rotate]"
-}
+//interface TypeChecking {
+//    fun walk() = "$name: walk"
+//    fun fly() = "$name: fly"
+//}
+//
+//class HouseFly : TypeChecking
+//
+//class Flea : Insect {
+//    override fun fly() =
+//        throw Exception("Flea cannot fly")
+//
+//    fun crawl() = "Flea: crawl"
+//}
+//
+//fun TypeChecking.basic() =
+//    walk() + " " +
+//            if (this is Flea)
+//                crawl()
+//            else
+//                fly()
+//
+//interface SwimmingInsect : TypeChecking {
+//    fun swim() = "$name: swim"
+//}
+//
+//interface WaterWalker : TypeChecking {
+//    fun walkWater() =
+//        "$name: walk on water"
+//}
+//
+//class WaterBeetle : SwimmingInsect
+//class WaterStrider : WaterWalker
+//class WhirligigBeetle :
+//    SwimmingInsect, WaterWalker
+//
+//fun TypeChecking.water() =
+//    when (this) {
+//        is SwimmingInsect -> swim()
+//        is WaterWalker -> walkWater()
+//        else -> "$name: drown"
+//    }
+//
+//fun main() {
+//    val insects = listOf(
+//        HouseFly(), Flea(), WaterStrider(),
+//        WaterBeetle(), WhirligigBeetle()
+//    )
+//    insects.map { it.basic() }  // =>
+//            "[HouseFly: walk HouseFly: fly, " +
+//            "Flea: walk Flea: crawl, " +
+//            "WaterStrider: walk WaterStrider: fly, " +
+//            "WaterBeetle: walk WaterBeetle: fly, " +
+//            "WhirligigBeetle: walk " +
+//            "WhirligigBeetle: fly]"
+//    insects.map { it.water() }  // =>
+//            "[HouseFly: drown, Flea: drown, " +
+//            "WaterStrider: walk on water, " +
+//            "WaterBeetle: swim, " +
+//            "WhirligigBeetle: swim]"
+//}
+//
+//interface Shape {
+//    fun draw(): String
+//}
+//
+//class Triangle : Shape {
+//    override fun draw() = "Triangle: Draw"
+//    fun rotate() = "Triangle: Rotate"
+//}
+//
+//fun turn2(s: Shape) = when (s) {
+//    is Square -> s.rotate()
+//    is Triangle -> s.rotate()
+//    else -> ""
+//}
+//
+//fun main() {
+//    val shapes =
+//        listOf(Circle(), Square(), Triangle())
+//    shapes.map { it.draw() }  // => "[Circle: Draw, Square: Draw, Triangle: Draw]"
+//    shapes.map { turn(it) }  // => "[, Square: Rotate, ]"
+//    shapes.map { turn2(it) }  // => "[, Square: Rotate, Triangle: Rotate]"
+//}
